99b10ba6532ebe24ec140375c3ab1ec9439ee59e
Danny Befumo <DPBefumo@gmail.com>
2020-06-17 21:21:07 -0400

    update

--- snippets/forms.py
+++ snippets/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Snippet
 
 class SnippetForm(forms.ModelForm):
+    tag_names = forms.CharField(label='Tags', help_text='Separate tags with a space', widget=forms.TextInput(attrs={'class': 'pa2 f4 w-90 bg-silver'}))
+
     class Meta:
         model = Snippet
         fields = [
@@ -9,7 +11,6 @@ class SnippetForm(forms.ModelForm):
             'description',
             'language',
             'body',
-            'tags',
             'visibility',
         ]
         widgets = {
@@ -17,6 +18,5 @@ class SnippetForm(forms.ModelForm):
             'description': forms.TextInput(attrs={'class': 'pa2 f4 w-80 bg-silver'}),
             'language': forms.Select(attrs={'class': 'pa2 f4 w-60 bg-silver'}),
             'body': forms.Textarea(attrs={'class': 'pa2 f4 h4 w-90 h5 bg-silver'}),
-            'tags': forms.SelectMultiple(attrs={'class': 'pa2 f4 w-60 bg-silver'}),
             'visibility': forms.Select(attrs={'class': 'pa2 f4 w-60 bg-silver'}),
         } 
